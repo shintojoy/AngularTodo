65484d7a7e6af3b63d927d6048c29095793ceef5
Shinto <shinto.scaria@equifax.com>
2017-05-03 19:01:52 +0530

    added list components

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+
+  constructor() { }
+  @Input()
+  todos: Todo[];
+
+  @Output()
+  remove: EventEmitter<Todo> = new EventEmitter();
+
+  @Output()
+  toggleComplete: EventEmitter<Todo> = new EventEmitter();
+
+  onToggleTodoComplete(todo: Todo) {
+    this.toggleComplete.emit(todo);
+  }
+
+  onRemoveTodo(todo: Todo) {
+    this.remove.emit(todo);
+  }
+
+}
